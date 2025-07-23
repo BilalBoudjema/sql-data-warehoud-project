c6f914fbc32079084bce936459b6f40ab092defd
Bilal Boudjema <45709231+BilalBoudjema@users.noreply.github.com>
2025-07-23 20:17:35 +0200

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,124 +1,167 @@
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
-	-- START OF PROCESS
-	PRINT '============================================================';
-	PRINT 'STARTING BRONZE LAYER LOAD';
-	PRINT '============================================================';
+	DECLARE @start_time DATETIME, @end_time DATETIME;
+	BEGIN TRY
+		-- START OF PROCESS
+		PRINT '============================================================';
+		PRINT 'STARTING BRONZE LAYER LOAD';
+		PRINT '============================================================';
 
-	-- CRM TABLES
-	PRINT '';
-	PRINT '------------------------------------------------------------';
-	PRINT 'SECTION: LOADING CRM TABLES';
-	PRINT '------------------------------------------------------------';
+		-- CRM TABLES
+		PRINT '';
+		PRINT '------------------------------------------------------------';
+		PRINT 'SECTION: LOADING CRM TABLES';
+		PRINT '------------------------------------------------------------';
 
-	PRINT '';
-	PRINT 'TABLE: bronze.crm_cust_info';
-	PRINT '------------------------------------------------------------';
-	PRINT 'Truncating existing data...';
-	TRUNCATE TABLE bronze.crm_cust_info;
-	PRINT 'Table truncated.';
-	PRINT 'Loading new data...';
-	BULK INSERT bronze.crm_cust_info
-	FROM 'C:\Users\Bilal\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
-	PRINT 'Data inserted.';
+		-- bronze.crm_cust_info
+		SET @start_time = GETDATE();
+		PRINT '';
+		PRINT 'TABLE: bronze.crm_cust_info';
+		PRINT '------------------------------------------------------------';
+		PRINT 'Truncating existing data...';
+		TRUNCATE TABLE bronze.crm_cust_info;
+		PRINT 'Table truncated.';
+		PRINT 'Loading new data...';
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Users\Bilal\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT 'Data inserted.';
+		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '--------------';
 
-	PRINT '';
-	PRINT 'TABLE: bronze.crm_prd_info';
-	PRINT '------------------------------------------------------------';
-	PRINT 'Truncating existing data...';
-	TRUNCATE TABLE bronze.crm_prd_info;
-	PRINT 'Table truncated.';
-	PRINT 'Loading new data...';
-	BULK INSERT bronze.crm_prd_info
-	FROM 'C:\Users\Bilal\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
-	PRINT 'Data inserted.';
+		-- bronze.crm_prd_info
+		SET @start_time = GETDATE();
+		PRINT '';
+		PRINT 'TABLE: bronze.crm_prd_info';
+		PRINT '------------------------------------------------------------';
+		PRINT 'Truncating existing data...';
+		TRUNCATE TABLE bronze.crm_prd_info;
+		PRINT 'Table truncated.';
+		PRINT 'Loading new data...';
+		BULK INSERT bronze.crm_prd_info
+		FROM 'C:\Users\Bilal\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT 'Data inserted.';
+		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '--------------';
 
-	PRINT '';
-	PRINT 'TABLE: bronze.crm_sales_details';
-	PRINT '------------------------------------------------------------';
-	PRINT 'Truncating existing data...';
-	TRUNCATE TABLE bronze.crm_sales_details;
-	PRINT 'Table truncated.';
-	PRINT 'Loading new data...';
-	BULK INSERT bronze.crm_sales_details
-	FROM 'C:\Users\Bilal\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
-	PRINT 'Data inserted.';
+		-- bronze.crm_sales_details
+		SET @start_time = GETDATE();
+		PRINT '';
+		PRINT 'TABLE: bronze.crm_sales_details';
+		PRINT '------------------------------------------------------------';
+		PRINT 'Truncating existing data...';
+		TRUNCATE TABLE bronze.crm_sales_details;
+		PRINT 'Table truncated.';
+		PRINT 'Loading new data...';
+		BULK INSERT bronze.crm_sales_details
+		FROM 'C:\Users\Bilal\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT 'Data inserted.';
+		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '--------------';
 
-	-- ERP TABLES
-	PRINT '';
-	PRINT '------------------------------------------------------------';
-	PRINT 'SECTION: LOADING ERP TABLES';
-	PRINT '------------------------------------------------------------';
+		-- ERP TABLES
+		PRINT '';
+		PRINT '------------------------------------------------------------';
+		PRINT 'SECTION: LOADING ERP TABLES';
+		PRINT '------------------------------------------------------------';
 
-	PRINT '';
-	PRINT 'TABLE: bronze.erp_cust_az12';
-	PRINT '------------------------------------------------------------';
-	PRINT 'Truncating existing data...';
-	TRUNCATE TABLE bronze.erp_cust_az12;
-	PRINT 'Table truncated.';
-	PRINT 'Loading new data...';
-	BULK INSERT bronze.erp_cust_az12
-	FROM 'C:\Users\Bilal\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
-	PRINT 'Data inserted.';
+		-- bronze.erp_cust_az12
+		SET @start_time = GETDATE();
+		PRINT '';
+		PRINT 'TABLE: bronze.erp_cust_az12';
+		PRINT '------------------------------------------------------------';
+		PRINT 'Truncating existing data...';
+		TRUNCATE TABLE bronze.erp_cust_az12;
+		PRINT 'Table truncated.';
+		PRINT 'Loading new data...';
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\Users\Bilal\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT 'Data inserted.';
+		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '--------------';
 
-	PRINT '';
-	PRINT 'TABLE: bronze.erp_loc_a101';
-	PRINT '------------------------------------------------------------';
-	PRINT 'Truncating existing data...';
-	TRUNCATE TABLE bronze.erp_loc_a101;
-	PRINT 'Table truncated.';
-	PRINT 'Loading new data...';
-	BULK INSERT bronze.erp_loc_a101
-	FROM 'C:\Users\Bilal\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
-	PRINT 'Data inserted.';
+		-- bronze.erp_loc_a101
+		SET @start_time = GETDATE();
+		PRINT '';
+		PRINT 'TABLE: bronze.erp_loc_a101';
+		PRINT '------------------------------------------------------------';
+		PRINT 'Truncating existing data...';
+		TRUNCATE TABLE bronze.erp_loc_a101;
+		PRINT 'Table truncated.';
+		PRINT 'Loading new data...';
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'C:\Users\Bilal\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT 'Data inserted.';
+		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '--------------';
 
-	PRINT '';
-	PRINT 'TABLE: bronze.erp_px_cat_g1v2';
-	PRINT '------------------------------------------------------------';
-	PRINT 'Truncating existing data...';
-	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-	PRINT 'Table truncated.';
-	PRINT 'Loading new data...';
-	BULK INSERT bronze.erp_px_cat_g1v2
-	FROM 'C:\Users\Bilal\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
-	PRINT 'Data inserted.';
+		-- bronze.erp_px_cat_g1v2
+		SET @start_time = GETDATE();
+		PRINT '';
+		PRINT 'TABLE: bronze.erp_px_cat_g1v2';
+		PRINT '------------------------------------------------------------';
+		PRINT 'Truncating existing data...';
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+		PRINT 'Table truncated.';
+		PRINT 'Loading new data...';
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\Users\Bilal\Desktop\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT 'Data inserted.';
+		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '--------------';
 
-	-- END OF PROCESS
-	PRINT '';
-	PRINT '============================================================';
-	PRINT 'BRONZE LAYER LOAD COMPLETED SUCCESSFULLY';
-	PRINT '============================================================';
-END;
+		-- END OF PROCESS
+		PRINT '';
+		PRINT '============================================================';
+		PRINT 'BRONZE LAYER LOAD COMPLETED SUCCESSFULLY';
+		PRINT '============================================================';
+
+	END TRY
+	BEGIN CATCH
+		PRINT '============================================================';
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER';
+		PRINT 'ERROR MESSAGE: ' + ERROR_MESSAGE();
+		PRINT 'ERROR NUMBER: ' + CAST(ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'ERROR STATE: ' + CAST(ERROR_STATE() AS NVARCHAR);
+		PRINT '============================================================';
+	END CATCH
+END
+
 
 	-- To execute procedure
 
